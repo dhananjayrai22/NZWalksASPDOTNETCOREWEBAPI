e50ceb5801b89301d4e1d74c4efe0359a43ac563
dhananjay22 <dhananjay.rai52@gmail.com>
2024-10-11 00:32:46 +0530

    Add project files.

---
+++ NZWalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Data;
+using NZWalksAPI.Models.Domain;
+using NZWalksAPI.Models.DTO;
+using NZWalksAPI.Models.DTOs;
+
+namespace NZWalksAPI.Controllers
+{
+    //https://localhost:1234/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        // GET ALL REGIONS
+        // GET: https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Get Data from Database - Domain models
+            var regionsDomain = dbContext.Regions.ToList();
+
+            // Map Domain Models to DTOs
+            var regionsDto = new List<RegionDto>();
+            foreach (var regionDomain in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = regionDomain.Id,
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+
+                });
+            }
+
+            // Return DTOs
+            return Ok(regionsDto);
+        }
+
+        // GET SINGLE REGION (Get Region By ID)
+        // GET: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //var region = dbContext.Regions.Find(id);
+            //Get Region Domain Model from Database
+            var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+
+            // Map/Convert Region Domain Model to Region DTO
+            // 
+
+            var regionDto = new RegionDto
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+
+            // Return DTO Back to client
+            return Ok(regionDto);
+        }
+
+        // POST To Create New Region
+        // POST: https://localhost:portnumber/api/regions
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            // Map or Convert DTO to Domain Model
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+
+            // Use Domain Model to create Region
+            dbContext.Regions.Add(regionDomainModel);
+            dbContext.SaveChanges();
+
+            // Map Domain model back to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+
+            return CreatedAtAction(nameof(GetById), new { id = regionDomainModel.Id }, regionDomainModel);
+        }
+    }
+}
+
+
+
+
+//var regions = new List<Region>
+//{
+//    new Region
+//    {
+//        Id = Guid.NewGuid(),
+//        Name = "Auckland Region",
+//        Code = "AKL",
+//        RegionImageUrl="https://www.pexels.com/photo/single-boat-on-a-sea-in-mist-11625522/"
+//    },
+//    new Region
+//    {
+//        Id = Guid.NewGuid(),
+//        Name = "Wellington Region",
+//        Code = "WLG",
+//        RegionImageUrl="https://www.pexels.com/photo/single-boat-on-a-sea-in-mist-11625522/"
+//    }
+//};
+
+
